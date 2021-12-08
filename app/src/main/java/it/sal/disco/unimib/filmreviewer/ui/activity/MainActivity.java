d6b0813c0ba3d9bed2b991ab9953d6a5b201767b
Edo_256 <edoardobianchi001@gmail.com>
2021-12-08 14:32:43 +0100

    Aggiunti fragment e relativo NavHost

--- app/src/main/java/it/sal/disco/unimib/filmreviewer/ui/activity/MainActivity.java
+++ app/src/main/java/it/sal/disco/unimib/filmreviewer/ui/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package it.sal.disco.unimib.filmreviewer.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import it.sal.disco.unimib.filmreviewer.R;
 
@@ -12,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment_news_act =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.main_fragmentContainerView);
+        NavController navController_news_act =
+                navHostFragment_news_act.getNavController();
+        BottomNavigationView bottomNavigView_news_act =
+                findViewById(R.id.main_bottom_nav_view);
+        NavigationUI
+                .setupWithNavController(bottomNavigView_news_act, navController_news_act);
+
     }
 }
