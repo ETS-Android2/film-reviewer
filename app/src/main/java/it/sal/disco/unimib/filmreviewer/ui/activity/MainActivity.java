653750e475e4dc0d0d0475a618511e54e0e99410
Edo_256 <edoardobianchi001@gmail.com>
2021-12-08 13:23:52 +0100

    Creazione progetto e main activity

---
+++ app/src/main/java/it/sal/disco/unimib/filmreviewer/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package it.sal.disco.unimib.filmreviewer.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.sal.disco.unimib.filmreviewer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
